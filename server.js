3c88ff69d1f2ef83d74a0369cbe144f4c55fc489
Jeff Anciado <jeffjustine7171@yahoo.com>
2022-11-15 22:19:00 +0800

    adding port

--- server.js
+++ server.js
@@ -47,8 +47,8 @@ app.get('/profile/:id', (req, res) => {handleProfile(req, res, db)})
 app.put('/image', (req, res) => {handleImage(req, res, db)})
 
 
-app.listen(3000, () => {
-    console.log("app is running on port 3000")
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`)
 })
 
 
